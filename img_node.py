6fe9896291e3850f9ee11be3ff1ed5c7ada39551
Carlos hurtado <carlosahs42@gmail.com>
2022-06-18 21:46:06 -0500

    Update area and height thresholds

--- img_node.py
+++ img_node.py
@@ -3,6 +3,7 @@
 Authors: Oscar D, Carlos H
 Description: ROS Noetic node to make prediction of traffic signals based on the YOLOv5s model
 """
+from calendar import day_abbr
 import numpy as np
 import torch
 import cv2
@@ -12,10 +13,11 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Int32
 import heapq
 
-SIGNAL_THRESHOLD_HI = 6500
-SIGNAL_THRESHOLD_LO = 3000
+SIGNAL_THRESHOLD_HI = 2000.0
+SIGNAL_THRESHOLD_LO = 650.0
 
-SEM_THRESHOLD = 1000
+SEM_THRESHOLD_LO = 37.0
+SEM_THRESHOLD_HI = 50.0
 
 class ImageDetect:
     def __init__(self):
@@ -28,11 +30,14 @@ class ImageDetect:
             "/video_low_res", Image, self.image_callback
         ) # video_source/raw - camera/image_raw - /video_low_res
         # Publisher        
-        self.sem_pub = rospy.Publisher("/sem_color", Int32, queue_size=1)
-        self.signal_pub = rospy.Publisher("/signal_detected", Int32, queue_size=1) 
+        self.sem_pub = rospy.Publisher("/sem_id", Int32, queue_size=1)
+        self.signal_pub = rospy.Publisher("/sig_id", Int32, queue_size=1) 
 
         # Variables para guardar la imagen y devolver las señales detectadas
         self.image_recieved = np.zeros((0,0))
+
+        self.signal = Int32()
+        self.semaphore = Int32()
         
         rate = 20
         r = rospy.Rate(rate)
@@ -53,43 +58,44 @@ class ImageDetect:
                 results = model(rgb)
 
                 data = results.pandas().xyxy[0]
-                data['area'] = abs(data['xmin'] - data['xmax']) * abs(data['ymin'] - data['ymax'])
+                data['x_len'] = abs(data['xmin'] - data['xmax'])
+                data['y_len'] = abs(data['ymin'] - data['ymax'])
                 data.drop(columns=['xmax', 'xmin', 'ymax', 'ymin'])
 
                 data = data.to_dict()
 
                 signals, semaphores = self._get_preds_pq(data)
+                # print(semaphores[0])
 
                 if len(signals) > 0:
                     # print(data)
-                    print(signals[0])
 
-                    if len(signals) > 0 and (
-                        -signals[0][0] <= SIGNAL_THRESHOLD_HI and -signals[0][0] >= SIGNAL_THRESHOLD_LO
-                    ):
-                        self.signal_pub.publish(signals[0][1])
+                    if -signals[0][0] <= SIGNAL_THRESHOLD_HI and -signals[0][0] >= SIGNAL_THRESHOLD_LO:
+                        self.signal = signals[0][1]
+                        self.signal_pub.publish(self.signal)
+                        # print(signals[0])
                     else:
                         self.signal_pub.publish(-1)
                 else:
                     self.signal_pub.publish(-1)
 
                 if len(semaphores) > 0:
-                    print(semaphores[0])
-
-                    if len(semaphores) > 0 and semaphores[0][0] >= SEM_THRESHOLD:
-                        self.sem_pub.publish(semaphores[0][1]) # Publish semaphore class
-                    else:
-                        self.sem_pub.publish(-1)
+                    if semaphores[0][0] >= SEM_THRESHOLD_LO and semaphores[0][0] <= SEM_THRESHOLD_HI:
+                        self.semaphore = semaphores[0][1]
+                        self.sem_pub.publish(self.semaphore) # Publish semaphore class
+                        # print(semaphores[0])
+                    # else:
+                    #     self.sem_pub.publish(-1)
                 else:
                     self.sem_pub.publish(-1)
 
-                # cv2.imshow('YOLO', np.squeeze(results.render())) 
-                # if cv2.waitKey(10) & 0xFF == ord('q'):
-                #     break
+                cv2.imshow('YOLO', np.squeeze(results.render())) 
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
             r.sleep()  
             
         image.release()
-        # cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
 
     def _get_preds_pq(self, data):
         num_signals = len(data["class"])
@@ -103,12 +109,13 @@ class ImageDetect:
             confidence = signal_map["confidence"]
             name = signal_map["name"]
             name_id = signal_map["class"]
-            area = signal_map["area"]
+            x_len = signal_map["x_len"]
+            y_len = signal_map["y_len"]
             # area = abs(xmin - xmax) * abs(ymin - ymax)
             if signal_map["name"].find("semaphore") >= 0:
-                semaphore_list.append((area, name_id, name, confidence))
+                semaphore_list.append((y_len, name_id, name, confidence))
             else:
-                signal_list.append((-area, name_id, name, confidence))
+                signal_list.append((-x_len * y_len, name_id, name, confidence))
         heapq.heapify(signal_list)
         heapq.heapify(semaphore_list)
         return signal_list, semaphore_list
